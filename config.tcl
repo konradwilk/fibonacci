d52d0d251d989491866b9ce1c1a049bcb20d57f2
Konrad Rzeszutek Wilk <konrad@kernel.org>
2021-05-22 15:35:22 +0200

    wrapper/fpga: Move them to src directory.
    Signed-off-by: Konrad Rzeszutek Wilk <konrad@kernel.org>

--- config.tcl
+++ config.tcl
@@ -4,7 +4,7 @@ set script_dir [file dirname [file normalize [info script]]]
 set ::env(DESIGN_NAME) wrapper_fibonacci
 
 # Change if needed
-set ::env(VERILOG_FILES) "./designs/fibonacci/wrapper.v \
+set ::env(VERILOG_FILES) "./designs/fibonacci/src/wrapper.v \
 	./designs/fibonacci/src/fibonacci.v \
 	./designs/fibonacci/src/wb_logic.v \
 	./designs/fibonacci/src/clkdiv.v"
