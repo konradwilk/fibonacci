33ccbc38f18a16b0432f2c410110839f87fc3f49
Konrad Rzeszutek Wilk <konrad@kernel.org>
2021-05-22 23:14:17 +0200

    verilog: Remove the old wishbone code
    .. which was moved to wb_logic.v file.
    
    Signed-off-by: Konrad Rzeszutek Wilk <konrad@kernel.org>

--- src/wrapper.v
+++ src/wrapper.v
@@ -84,87 +84,6 @@ module wrapper_fibonacci  (
     wire fibonacci_switch;
     wire [5:0] clocks;
 
-/*
-    wire wb_active = wbs_stb_i & wbs_cyc_i;
-    reg [31:0] buffer;
-    reg [31:0] buffer_o;
-
-
-    
-    localparam CTRL_GET_NR		= 'h00;
-    localparam CTRL_NR 			= 'h8;
-
-    localparam CTRL_GET_ID		= 'h04;
-    localparam CTRL_ID			= 32'h4669626f;
-
-    localparam CTRL_SET_IRQ		= 'h08;
-    localparam ACK_OK			= 32'h0000001;
-    localparam ACK_OFF			= 32'h0000000;
-    localparam CTRL_CLOCK		= 'h10;
-    localparam CTRL_FIBONACCI_CTRL 	= 'h0C;
-    localparam TURN_ON			= 1'b1;
-    localparam TURN_OFF			= 1'b0;
-    localparam CTRL_FIBONACCI_VAL	= 'h14;
-    localparam CTRL_WRITE	  	= 'h18;
-    localparam CTRL_READ	  	= 'h1C;
-    localparam CTRL_PANIC	  	= 'h20;
-
-    always @(posedge wb_clk_i) begin
-	    if (reset) begin
-		    fibonacci_switch <= 1'b1;
-		    buffer_o <= ACK_OFF;
-		    clock_op <= 6'b000001;
-	    end else begin
-		    if (wb_active && !wbs_we_i && (wbs_adr_i[32:5] == BASE_ADDRESS)) begin
-			    case (wbs_adr_i[5:0])
-				    CTRL_GET_NR:
-					    buffer_o <= CTRL_NR;
-				    CTRL_GET_ID:
-					    buffer_o <= CTRL_ID;
-				    CTRL_SET_IRQ:
-					    buffer_o <= ACK_OK;
-				    CTRL_CLOCK:
-					    clock_op <= wbs_dat_i[5:0];
-				    CTRL_FIBONACCI_CTRL:
-					    fibonacci_switch <= wbs_dat_i[0];
-				    CTRL_FIBONACCI_VAL:
-					    buffer_o <= {2'h0, buf_io_out[37:8]};
-				    CTRL_READ:
-					    buffer_o <= buffer;
-			             default:
-					    buffer_o <= ACK_OFF;
-				endcase
-		     end
-	     end
-     end
-
-     always @(posedge wb_clk_i) begin
-	     if (reset) begin
-		     buffer <= ACK_OFF;
-	     end else begin
-		     
-		     if (wb_active && wbs_we_i && &wbs_sel_i &&
-			 (wbs_adr_i[32:5] == BASE_ADDRESS)) begin
-			     case (wbs_adr_i[5:0])
-				     CTRL_WRITE:
-					     buffer <= wbs_dat_i;
-				     CTRL_PANIC:
-					     buffer <= wbs_dat_i;
-				     default:
-					     buffer <= ACK_OFF;
-			     endcase
-		     end
-	     end
-     end
-
-     assign buf_wbs_ack_o = reset ? 1'b0 : (wb_active &&
-					   ((wbs_adr_i[32:5] == BASE_ADDRESS) &&
-					    (wbs_adr_i[5:0] <= CTRL_PANIC)));
-
-    assign buf_wbs_dat_o = reset ? 32'b0 : buffer_o;
-*/
-    assign clocks[0] = wb_clk_i;
-
     clkdiv #(.WIDTH(8)) Clock_1 (
 	    .clk(wb_clk_i),
 	    .clkout(clocks[1]));
@@ -201,6 +120,8 @@ module wrapper_fibonacci  (
     	    .wbs_adr_i(wbs_adr_i),
     	    .wbs_ack_o(buf_wbs_ack_o),
     	    .wbs_dat_o(buf_wbs_dat_o));
+
+    assign clocks[0] = wb_clk_i;
 
     assign fibonacci_clock = clock_op[5] ? clocks[5] :
 	    			(clock_op[4] ? clocks[4] :
