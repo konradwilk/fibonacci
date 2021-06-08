fd369961cc78838a37868bc07560210d88546044
Matt Venn <matt@mattvenn.net>
2021-06-08 10:10:05 +0200

    changes for userclock2

--- src/wrapper.v
+++ src/wrapper.v
@@ -43,6 +43,9 @@ module wrapper_fibonacci  (
     // IRQ
     output wire [2:0] irq,
 
+    // extra user clock
+    input wire user_clock2,
+
     // active input, only connect tristated outputs if this is high
     input wire active
 );
